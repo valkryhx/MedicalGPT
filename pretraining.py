07b7fdb446384366dd640f3d10ce9e2d84dd0316
valkryhx <283306449@qq.com>
2023-07-11 20:26:49 +0800

    Update pretraining.py

--- pretraining.py
+++ pretraining.py
@@ -28,7 +28,7 @@ import numpy as np
 import torch
 from datasets import load_dataset
 from loguru import logger
-from peft import {
+from peft import (
     LoraConfig, 
     TaskType, 
     get_peft_model, 
@@ -36,8 +36,7 @@ from peft import {
     prepare_model_for_int8_training,
     prepare_model_for_kbit_training,
     set_peft_model_state_dict,
-    
-}
+)
 from sklearn.metrics import accuracy_score
 from transformers import (
     BloomForCausalLM,
