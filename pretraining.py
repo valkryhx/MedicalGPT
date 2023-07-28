939677f626076cd9be07cec501b606382931e28e
valkryhx <283306449@qq.com>
2023-07-28 22:46:31 +0800

    Update pretraining.py

--- pretraining.py
+++ pretraining.py
@@ -302,7 +302,7 @@ class SavePeftModelTrainer_old(Trainer):
         logger.info("model saved during  SavePeftModelTrainer.traininig")
         #torch.save(self.args, os.path.join(output_dir, TRAINING_ARGS_NAME))
         ############torch.save(self.args, os.path.join(output_dir, "training_args.bin"))  老子把这行注释总行了吧
-        logger.info("training args saved during  SavePeftModelTrainer.traininig")
+        logger.info("DO NOT SAVE THE FXXKING HUGE training args of deepspeed in SavePeftModelTrainer.traininig")
 
 class SavePeftModelTrainer(Trainer):
     print("save !!!!!!")
