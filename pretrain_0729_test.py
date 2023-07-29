31a6a8280218a4fda24530a364e46e9a2c879648
valkryhx <283306449@qq.com>
2023-07-29 20:14:48 +0800

    Update pretrain_0729_test.py

--- pretrain_0729_test.py
+++ pretrain_0729_test.py
@@ -50,77 +50,77 @@ from transformers import (
 from transformers.trainer import TRAINING_ARGS_NAME
 from transformers.utils.versions import require_version
 
-MODEL_CLASSES = {
-    "bloom": (AutoConfig, BloomForCausalLM, BloomTokenizerFast),
-    "chatglm": (AutoConfig, AutoModel, AutoTokenizer),
-    "llama": (AutoConfig, LlamaForCausalLM, LlamaTokenizer),
-    "baichuan": (AutoConfig, AutoModelForCausalLM, AutoTokenizer),
-    "auto": (AutoConfig, AutoModelForCausalLM, AutoTokenizer),
-}
+# MODEL_CLASSES = {
+#     "bloom": (AutoConfig, BloomForCausalLM, BloomTokenizerFast),
+#     "chatglm": (AutoConfig, AutoModel, AutoTokenizer),
+#     "llama": (AutoConfig, LlamaForCausalLM, LlamaTokenizer),
+#     "baichuan": (AutoConfig, AutoModelForCausalLM, AutoTokenizer),
+#     "auto": (AutoConfig, AutoModelForCausalLM, AutoTokenizer),
+# }
 
 
-@dataclass
-class ModelArguments:
-    """
-    Arguments pertaining to which model/config/tokenizer we are going to fine-tune, or train from scratch.
-    """
+# @dataclass
+# class ModelArguments:
+#     """
+#     Arguments pertaining to which model/config/tokenizer we are going to fine-tune, or train from scratch.
+#     """
 
-    model_type: str = field(
-        default=None,
-        metadata={"help": "Model type selected in the list: " + ", ".join(MODEL_CLASSES.keys())}
-    )
-    model_name_or_path: Optional[str] = field(
-        default=None,
-        metadata={
-            "help": (
-                "The model checkpoint for weights initialization.Don't set if you want to train a model from scratch."
-            )
-        },
-    )
-    tokenizer_name_or_path: Optional[str] = field(
-        default=None,
-        metadata={
-            "help": (
-                "The tokenizer for weights initialization.Don't set if you want to train a model from scratch."
-            )
-        },
-    )
-    load_in_4bit: bool = field(default=False, metadata={"help": "Very important to play on kaggle!!!Whether to load the model in 4bit mode or not."})
-    load_in_8bit: bool = field(default=False, metadata={"help": "Whether to load the model in 8bit mode or not."})
-    cache_dir: Optional[str] = field(
-        default=None,
-        metadata={"help": "Where do you want to store the pretrained models downloaded from huggingface.co"},
-    )
-    use_fast_tokenizer: bool = field(
-        default=False,
-        metadata={"help": "Whether to use one of the fast tokenizer (backed by the tokenizers library) or not."},
-    )
-    torch_dtype: Optional[str] = field(
-        default=None,
-        metadata={
-            "help": (
-                "Override the default `torch.dtype` and load the model under this dtype. If `auto` is passed, the "
-                "dtype will be automatically derived from the model's weights."
-            ),
-            "choices": ["auto", "bfloat16", "float16", "float32"],
-        },
-    )
-    device_map: Optional[str] = field(
-        default="auto",
-        metadata={"help": "Device to map model to. If `auto` is passed, the device will be selected automatically. "},
-    )
-    trust_remote_code: bool = field(
-        default=True,
-        metadata={"help": "Whether to trust remote code when loading a model from a remote checkpoint."},
-    )
+#     model_type: str = field(
+#         default=None,
+#         metadata={"help": "Model type selected in the list: " + ", ".join(MODEL_CLASSES.keys())}
+#     )
+#     model_name_or_path: Optional[str] = field(
+#         default=None,
+#         metadata={
+#             "help": (
+#                 "The model checkpoint for weights initialization.Don't set if you want to train a model from scratch."
+#             )
+#         },
+#     )
+#     tokenizer_name_or_path: Optional[str] = field(
+#         default=None,
+#         metadata={
+#             "help": (
+#                 "The tokenizer for weights initialization.Don't set if you want to train a model from scratch."
+#             )
+#         },
+#     )
+#     load_in_4bit: bool = field(default=False, metadata={"help": "Very important to play on kaggle!!!Whether to load the model in 4bit mode or not."})
+#     load_in_8bit: bool = field(default=False, metadata={"help": "Whether to load the model in 8bit mode or not."})
+#     cache_dir: Optional[str] = field(
+#         default=None,
+#         metadata={"help": "Where do you want to store the pretrained models downloaded from huggingface.co"},
+#     )
+#     use_fast_tokenizer: bool = field(
+#         default=False,
+#         metadata={"help": "Whether to use one of the fast tokenizer (backed by the tokenizers library) or not."},
+#     )
+#     torch_dtype: Optional[str] = field(
+#         default=None,
+#         metadata={
+#             "help": (
+#                 "Override the default `torch.dtype` and load the model under this dtype. If `auto` is passed, the "
+#                 "dtype will be automatically derived from the model's weights."
+#             ),
+#             "choices": ["auto", "bfloat16", "float16", "float32"],
+#         },
+#     )
+#     device_map: Optional[str] = field(
+#         default="auto",
+#         metadata={"help": "Device to map model to. If `auto` is passed, the device will be selected automatically. "},
+#     )
+#     trust_remote_code: bool = field(
+#         default=True,
+#         metadata={"help": "Whether to trust remote code when loading a model from a remote checkpoint."},
+#     )
 
-    def __post_init__(self):
-        if self.model_type is None:
-            raise ValueError(
-                "You must specify a valid model_type to run training. Available model types are " + ", ".join(
-                    MODEL_CLASSES.keys()))
-        if self.model_name_or_path is None:
-            raise ValueError("You must specify a valid model_name_or_path to run training.")
+#     def __post_init__(self):
+#         if self.model_type is None:
+#             raise ValueError(
+#                 "You must specify a valid model_type to run training. Available model types are " + ", ".join(
+#                     MODEL_CLASSES.keys()))
+#         if self.model_name_or_path is None:
+#             raise ValueError("You must specify a valid model_name_or_path to run training.")
 
 
 @dataclass
@@ -483,7 +483,7 @@ def main():
     tokenizer_name_or_path = "THUDM/chatglm2-6b" #model_args.tokenizer_name_or_path
     if not tokenizer_name_or_path:
         tokenizer_name_or_path = "THUDM/chatglm2-6b" #model_args.model_name_or_path
-    tokenizer = tokenizer_class.from_pretrained(tokenizer_name_or_path, **tokenizer_kwargs)
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_name_or_path, **tokenizer_kwargs)
 
     if training_args.use_peft:
         if training_args.peft_path is not None:
