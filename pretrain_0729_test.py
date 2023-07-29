aa3ad3bc24e70dbcf5cf32231415414c7c345115
valkryhx <283306449@qq.com>
2023-07-29 21:59:35 +0800

    Update pretrain_0729_test.py

--- pretrain_0729_test.py
+++ pretrain_0729_test.py
@@ -52,78 +52,6 @@ from transformers.utils.versions import require_version
 
 
 
-
-
-
-
-@dataclass
-class DataTrainingArguments:
-    """
-    Arguments pertaining to what data we are going to input our model for training and eval.
-    """
-
-    dataset_name: Optional[str] = field(
-        default=None, metadata={"help": "The name of the dataset to use (via the datasets library)."}
-    )
-    dataset_config_name: Optional[str] = field(
-        default=None, metadata={"help": "The configuration name of the dataset to use (via the datasets library)."}
-    )
-    train_file_dir: Optional[str] = field(default=None, metadata={"help": "The train text data file folder."})
-    validation_file_dir: Optional[str] = field(
-        default=None,
-        metadata={"help": "An optional input evaluation data file to evaluate the perplexity on text file folder."},
-    )
-    max_train_samples: Optional[int] = field(
-        default=None,
-        metadata={
-            "help": (
-                "For debugging purposes or quicker training, truncate the number of training examples to this "
-                "value if set."
-            )
-        },
-    )
-    max_eval_samples: Optional[int] = field(
-        default=None,
-        metadata={
-            "help": (
-                "For debugging purposes or quicker training, truncate the number of evaluation examples to this "
-                "value if set."
-            )
-        },
-    )
-    streaming: bool = field(default=False, metadata={"help": "Enable streaming mode"})
-    block_size: Optional[int] = field(
-        default=1024,
-        metadata={
-            "help": (
-                "Optional input sequence length after tokenization. "
-                "The training dataset will be truncated in block of this size for training. "
-                "Default to the model max input length for single sentence inputs (take into account special tokens)."
-            )
-        },
-    )
-    overwrite_cache: bool = field(
-        default=False, metadata={"help": "Overwrite the cached training and evaluation sets"}
-    )
-    validation_split_percentage: Optional[float] = field(
-        default=0.05,
-        metadata={
-            "help": "The percentage of the train set used as validation set in case there's no validation split"
-        },
-    )
-    preprocessing_num_workers: Optional[int] = field(
-        default=None,
-        metadata={"help": "The number of processes to use for the preprocessing."},
-    )
-    keep_linebreaks: bool = field(
-        default=True, metadata={"help": "Whether to keep line breaks when using TXT files or not."}
-    )
-
-    def __post_init__(self):
-        if self.streaming:
-            require_version("datasets>=2.0.0", "The streaming feature requires `datasets>=2.0.0`")
-
-
 @dataclass
 class PeftArguments(TrainingArguments):
     use_peft: bool = field(default=True, metadata={"help": "Whether to use peft"})
@@ -279,7 +207,7 @@ def main():
     #parser = HfArgumentParser((DataTrainingArguments, PeftArguments))
     parser = HfArgumentParser((PeftArguments))
     #parser = HfArgumentParser((TrainingArguments))
-    training_args , = parser.parse_args_into_dataclasses()
+    training_args , = parser.parse_json_file(json_file="luzi.json")
     logger.info(f"TYPE_OF_training_args = {type(training_args)}")
     training_args.qlora = True
     training_args.use_peft =True
