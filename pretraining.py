ee834f51a4562a2846ec8ffd0c76f2a5c700e89f
valkryhx <283306449@qq.com>
2023-07-30 01:03:57 +0800

    Update pretraining.py

--- pretraining.py
+++ pretraining.py
@@ -398,7 +398,7 @@ def find_all_linear_names_old(peft_model, int4=False, int8=False):
 def main():
     #parser = HfArgumentParser((ModelArguments, DataTrainingArguments, PeftArguments)) ## modify
     parser = HfArgumentParser((ModelArguments, DataTrainingArguments))  ##add 
-    model_args, data_args, _ = parser.parse_args_into_dataclasses()  ## modify training_args 不能来自命令行参数
+    model_args, data_args = parser.parse_args_into_dataclasses()  ## modify training_args 不能来自命令行参数
     training_args_parser =  HfArgumentParser(PeftArguments)  ## ADD 注意这是个tuple 虽然只有一个元素 但是要加逗号才能正常解析成PeftArguments
     training_args , = training_args_parser.parse_json_file(json_file="luzi.json")  ## ADD
     training_args.deepspeed = model_args.deepspeed  ##add 命令行传入的deepspeed参数先让model arg接住 再传给trainingargs
