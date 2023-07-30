ecd45212b88619a812b7095f7938b8c77ff2a72e
valkryhx <283306449@qq.com>
2023-07-30 19:21:02 +0800

    Update supervised_finetuning.py

--- supervised_finetuning.py
+++ supervised_finetuning.py
@@ -754,19 +754,19 @@ def main():
             
             if cur_len < tokenizer.model_max_length:
                 if cur_len == total_len:
-                    logger.warning(f"tokenization right match: {cur_len} vs. {total_len}. (ok)")
-                    logger.info(f"valid conversation is = {conversation}")
+                    #logger.warning(f"tokenization right match: {cur_len} vs. {total_len}. (ok)")   # too many  do not print info
+                    #logger.info(f"valid conversation is = {conversation}")
                     #raise ValueError("test valid")
                     count_ok += 1
                 
                 elif cur_len != total_len:
                     target[:] = IGNORE_INDEX
                     logger.warning(f"tokenization mismatch: {cur_len} vs. {total_len}. (ignored)")
-                    logger.warning(f"bug conversation is = {conversation}")
+                    #logger.warning(f"bug conversation is = {conversation}")
                     #raise ValueError("test bug")
                     count_bad += 1
-            if count_ok>5 and count_bad >5 :
-                raise Error("stop here")
+            #if count_ok>5 and count_bad >5 :
+            #    raise ValueError("stop here")
                     
 
         return dict(
