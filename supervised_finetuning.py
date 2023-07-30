42a28981029516443a645690e93fe09a3229eec1
valkryhx <283306449@qq.com>
2023-07-30 15:05:22 +0800

    Update supervised_finetuning.py

--- supervised_finetuning.py
+++ supervised_finetuning.py
@@ -747,11 +747,15 @@ def main():
                 cur_len += turn_len
 
             target[cur_len:] = IGNORE_INDEX
-
+            
             if cur_len < tokenizer.model_max_length:
                 if cur_len != total_len:
                     target[:] = IGNORE_INDEX
                     logger.warning(f"tokenization mismatch: {cur_len} vs. {total_len}. (ignored)")
+                    logger.warning(f"bug turn is = {turn}")
+                    raise ValueError("test bug turn")
+                
+                    
 
         return dict(
             input_ids=input_ids,
