892829aa40b224f38d18a89a8162c189c80e44c3
valkryhx <283306449@qq.com>
2023-07-30 18:32:39 +0800

    Update 0730_sft_test.py

--- 0730_sft_test.py
+++ 0730_sft_test.py
@@ -901,7 +901,7 @@ def main():
                 load_in_4bit=True,
                 bnb_4bit_use_double_quant=True,
                 bnb_4bit_quant_type="nf4",
-                bnb_4bit_compute_dtype=torch_dtype,
+                bnb_4bit_compute_dtype=torch.float16 , ,#torch_dtype,
                 ) if training_args.qlora else None,
             )
         else :
@@ -917,7 +917,7 @@ def main():
                 load_in_4bit=True,
                 bnb_4bit_use_double_quant=True,
                 bnb_4bit_quant_type="nf4",
-                bnb_4bit_compute_dtype=torch_dtype,
+                bnb_4bit_compute_dtype=torch.float16 ,, #torch_dtype,
                 ) if training_args.qlora else None,
             )
         if hasattr(model, 'lm_head'):
